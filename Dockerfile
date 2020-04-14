d1469b5651371e3a410abfb6238e30263b785d4f
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-04-14 17:45:15 -0500

    [initial] initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM alpine
+RUN apk update && apk add nodejs
+COPY . /app
+WORKDIR /app
+CMD ["node","index.js"]
+
+
---
+++ index.js
@@ -0,0 +1,3 @@
+var os = require("os");
+var hostname = os.hostname();
+console.log("hello from " + hostname);console.log("this is v0.2");
